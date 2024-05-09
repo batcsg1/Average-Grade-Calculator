62e6928256411d56bade26084e515a6e9976526b
SamBat2005 <batcsg1@student.op.ac.nz>
2024-05-10 11:26:28 +1200

    Updated all other files

--- AverageGradeCalculator/obj/Debug/net8.0/AverageGradeCalculator.AssemblyInfo.cs
+++ AverageGradeCalculator/obj/Debug/net8.0/AverageGradeCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AverageGradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e841f830c9d9155ac349e124e48f8ebe91c40715")]
 [assembly: System.Reflection.AssemblyProductAttribute("AverageGradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AverageGradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
